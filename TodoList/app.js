c38131aa4cc548f0b731997b35651c280da01cfa
Seungwook Lee <35110824+tmdenddl@users.noreply.github.com>
2021-01-10 13:07:41 -0800

    Refactor ToDo List Web App

--- TodoList/app.js
+++ TodoList/app.js
@@ -1,38 +1,54 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
 // Tell the app to use EJS as view engine
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 const port = 3000;
 
-let items = [];
+const items = [];
+const workItems = [];
 
 app.get("/", function(req, res) {
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
+  let day = date.getDate();
 
   // Render a file in list directory with ejs extension
   // Give 'kindOfDay' variable a value same as that of the 'day'
-  res.render("list", {kindOfDay: day, newListItems: items});
+  res.render("list", {listTitle: day, newListItems: items});
 });
 
 app.post("/", function(req, res) {
   let item = req.body.newItem;
-  items.push(item);
 
-  res.redirect("/");
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 });
+
+app.get("/work", function(req, res) {
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.post("/work", function(req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+
+  res.redirect("/work");
+});
+
+app.get("/about", function(req, res) {
+  res.render("about");
+});
+
 
 app.listen(port, function() {
   console.log("Server started on port " + port + ".");

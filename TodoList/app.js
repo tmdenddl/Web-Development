c10c251adf700732143203834fe2309d7e023d7e
Seungwook Lee <35110824+tmdenddl@users.noreply.github.com>
2021-01-10 10:47:20 -0800

    EJS Template added for ToDo List

--- TodoList/app.js
+++ TodoList/app.js
@@ -3,15 +3,35 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+// Tell the app to use EJS as view engine
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
 const port = 3000;
 
+let items = [];
+
 app.get("/", function(req, res) {
-  var today = new Date();
-  if(today.getDay() === 6 || today.getDay() === 0) {
-    res.sendFile(__dirname + "/weekend.html");
-  } else {
-    res.sendFile(__dirname + "/weekday.html");
-  }
+  let today = new Date();
+
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  let day = today.toLocaleDateString("en-US", options);
+
+  // Render a file in list directory with ejs extension
+  // Give 'kindOfDay' variable a value same as that of the 'day'
+  res.render("list", {kindOfDay: day, newListItems: items});
+});
+
+app.post("/", function(req, res) {
+  let item = req.body.newItem;
+  items.push(item);
+
+  res.redirect("/");
 });
 
 app.listen(port, function() {

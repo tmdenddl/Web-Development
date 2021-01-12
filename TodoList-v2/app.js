1da6df2b731b86715ef965471423f3b4c331ea7f
Seungwook Lee <35110824+tmdenddl@users.noreply.github.com>
2021-01-11 19:02:49 -0800

    Fixing syntax errors

--- TodoList-v2/app.js
+++ TodoList-v2/app.js
@@ -138,7 +138,7 @@ app.get("/about", function(req, res){
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
-});
+}
 
 app.listen(port, function() {
   console.log("Server started on port " + port + ".");

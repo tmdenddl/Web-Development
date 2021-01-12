be46b9837eaecb9d55b76c827d64ee25db19427a
Seungwook Lee <35110824+tmdenddl@users.noreply.github.com>
2021-01-11 23:13:51 -0800

    Initial Commit for Secret App

---
+++ Secrets/app.js
@@ -0,0 +1,54 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+const port = 3000;
+
+app.get("/", function(req, res) {
+  res.render("home");
+});
+
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+
+app.get("/register", function(req, res) {
+  res.render("register");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, function() {
+  console.log("Server started on port " + port + ".");
+});

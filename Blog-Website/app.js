7e84b96ec97cc4444e717b7db2f432eaaddc375b
Seungwook Lee <35110824+tmdenddl@users.noreply.github.com>
2021-01-11 19:15:16 -0800

    Blog-Website updated (adding mongoDB database)

--- Blog-Website/app.js
+++ Blog-Website/app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -11,7 +12,15 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
-let posts = [];
+mongoose.connect("mongodb+srv://admin-kevin:test123@cluster0.qcb3t.mongodb.net/blog", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
 
 app.set('view engine', 'ejs');
 
@@ -19,9 +28,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts
+  Post.find({}, function(err, posts) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("home", {
+        startingContent: homeStartingContent,
+        posts: posts
+      });
+    }
   });
 });
 
@@ -38,41 +53,35 @@ app.get("/compose", function(req, res) {
 });
 
 app.post("/compose", function(req, res) {
-  const post = {
+  const post =  new Post ({
     title: req.body.postTitle,
     content: req.body.postBody
-  };
-  posts.push(post);
+  });
+  post.save(function(err) {
+    if(!err) {
+      res.redirect("/");
+    }
+  });
 
-  res.redirect("/");
+  // res.redirect("/");
 });
 
-app.get("/posts/:postName", function(req, res) {
-  const requestedTitle = _.lowerCase(req.params.postName);
+app.get("/posts/:postId", function(req, res) {
+  const requestedPostId = _.lowerCase(req.params.postId);
 
-  posts.forEach(function(post) {
-    const storedTitle = _.lowerCase(post.title);
-    if (storedTitle === requestedTitle) {
-      console.log("Match Found!");
-      // render post.ejs template with the variables passed in
-      res.render("post", {
-        title: post.title,
-        content: post.content});
-    } else {
-      console.log("Match Not Found!");
-    }
+  Post.findOne({_id: requestedPostId}, function(err, post) {
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
   });
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-
-
-
-
-
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server started on port " + port + ".");
 });

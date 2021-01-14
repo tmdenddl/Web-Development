9f6076a78e6c8d87b6596af70ddc8a293461dc1b
Seungwook Lee <35110824+tmdenddl@users.noreply.github.com>
2021-01-13 21:02:52 -0800

    Level2 Encryption

---
+++ Secrets/README.md
@@ -0,0 +1,11 @@
+This is a Secrets web application. Users can authenticate themselves after registering using email and password. Secrets will only be visible to the user who posted them.
+
+This project will cover from Level 1 encryption to Level 6 encryption.
+Level 1: Username & Password
+Level 2: Database Encryption
+Level 3: Hashing Passwords
+Level 4: Salting and Hashing Passwords with bcrypt
+Level 5: Passport.js to add Cookies and Sessions
+Level 6: OAuth 2.0 & Sign-in with Google
+
+This application utilizes Node.js, Express, MongoDB and REST API.
--- Secrets/app.js
+++ Secrets/app.js
@@ -1,8 +1,12 @@
 //jshint esversion:6
 
+require('dotenv').config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -10,7 +14,17 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
-const port = 3000;
+// mongoose.connect("mongodb+srv://admin-kevin:test123@cluster0.qcb3t.mongodb.net/wikiDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true });
+
+const userSchema = new mongoose.Schema ({
+  email: String,
+  password: String
+});
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", function(req, res) {
   res.render("home");
@@ -24,30 +38,43 @@ app.get("/register", function(req, res) {
   res.render("register");
 });
 
+app.post("/register", function(req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save(function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
+
+app.post("/login", function(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: username}, function(err, foundUser) {
+    if(err) {
+      console.log(err);
+    } else {
+      if(foundUser && foundUser.password === password) {
+        res.render("secrets");
+      }
+    }
+  })
+});
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//====================== Server Port Related ======================//
+// Listen on default port specified by MongoDB, otherwise 3000
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 app.listen(port, function() {
   console.log("Server started on port " + port + ".");
